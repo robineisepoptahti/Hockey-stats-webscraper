d51723fe33ef6217fbe9a887e22e4a1990db4a80
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2026-02-18 20:51:58 +0200

    playwright working

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from "@playwright/test";
 
+const isMacArm64 = process.platform === "darwin" && process.arch === "arm64";
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -26,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: "http://127.0.0.1:4173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -44,10 +46,14 @@ export default defineConfig({
       use: { ...devices["Desktop Firefox"] },
     },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
+    ...(!isMacArm64
+      ? [
+          {
+            name: "webkit",
+            use: { ...devices["Desktop Safari"] },
+          },
+        ]
+      : []),
 
     /* Test against mobile viewports. */
     // {
--- frontend/tests/home.test.ts
+++ frontend/tests/home.test.ts
@@ -1,7 +1,5 @@
 import { test, expect } from "@playwright/test";
 
-//const baseUrl = "http://127.0.0.1:5173/";
-
 test("should display the home page", async ({ page }) => {
   await page.goto("/");
   await expect(page).toHaveTitle(/frontend/);

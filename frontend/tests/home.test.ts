4685e6deebf7cfabbaa9d88d762f348616b6db04
robineisepoptahti <155745755+robineisepoptahti@users.noreply.github.com>
2026-02-18 18:45:51 +0200

    Addeing

--- frontend/tests/home.test.ts
+++ frontend/tests/home.test.ts
@@ -1,8 +1,8 @@
 import { test, expect } from "@playwright/test";
 
-const baseUrl = "http://127.0.0.1:5173/";
+//const baseUrl = "http://127.0.0.1:5173/";
 
 test("should display the home page", async ({ page }) => {
-  await page.goto(baseUrl);
+  await page.goto("/");
   await expect(page).toHaveTitle(/frontend/);
 });
